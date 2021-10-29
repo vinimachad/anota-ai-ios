452d15a3cdf441e3c89a53b8cdf44d95d0bbc004
vinicius galhardo <viniciusgm45@gmail.com>
2021-10-28 20:38:59 -0400

    Initial Commit

---
+++ AnotaAi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnotaAi
+//
+//  Created by Vinicius Galhardo Machado on 28/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
